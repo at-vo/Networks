f52028f314bbdbaaa8e20dd321c87c20b2795740
Dat Vo <dat109@ymail.com>
2019-11-22 17:13:20 -0500

    done

--- CS3357/asn3/UDP_client.py
+++ CS3357/asn3/UDP_client.py
@@ -24,12 +24,13 @@ def main():
     seq = 0
 
     for data in packet_data:
-        UDP_Packet = createPacket(ack,seq,data.encode())
+        chksum = createChksum(ack,seq,data.encode())
+        UDP_Packet = createPacket(ack,seq,data.encode(),chksum)
         #Send the UDP Packet
         sock = socket.socket(socket.AF_INET, # Internet
                             socket.SOCK_DGRAM) # UDP
         sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
-        print("packet sent\n")
+        print("packet sent:", UDP_Packet)
 
         # imitate do while loop
         while True:
@@ -37,21 +38,25 @@ def main():
                 sock.settimeout(CONST_TIMEOUT)
                 #response from server
                 data, serverAddress = sock.recvfrom(1024)
-                unpacker = struct.Struct('I I 32s') # (ack, seq, chksum)
+                unpacker = struct.Struct('I I 8s 32s') # (ack, seq, data, chksum)
                 server_reponse = unpacker.unpack(data) 
                 print("response received:", server_reponse)
                 #assign references
                 response_ack = server_reponse[0]
                 response_seq = server_reponse[1]
-                response_chksum = server_reponse[2]
-
-                if response_ack!=ack:
-                    if (response_seq != seq):
-                        UDP_Packet = createPacket(ack,seq,data) # create packet
-                        sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT)) # send packet again
-                        sock.settimeout(CONST_TIMEOUT) # reset timer
-                    else:
-                        break
+                response_data = server_reponse[2]
+                response_chksum = server_reponse[3]
+                check = createChksum(response_ack,response_seq,response_data)
+                if check == response_chksum:
+                    if response_ack!=ack:
+                        if (response_seq != seq):
+                            sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT)) # send packet again
+                            print("packet resent:", UDP_Packet)
+                        else:
+                            break
+                else:
+                    sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT)) # send packet again
+                    print("checksum corrupt. packet resent:",UDP_Packet)
             except socket.timeout as err:
                 print("timeout\n")
                 sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT)) # send packet again
@@ -63,19 +68,21 @@ def main():
 
     sock.close()
 
-def createPacket(ack,seq,data):
+def createPacket(ack,seq,data,chksum):                                                                                                                                
+    #Build the UDP Packet
+    values = (ack,seq,bytes(data),chksum)
+    UDP_Packet_Data = struct.Struct('I I 8s 32s')
+    UDP_Packet = UDP_Packet_Data.pack(*values)
+    return UDP_Packet
+
+def createChksum(ack,seq,data):
     #Create the Checksum
     bytes_data = bytes(data)
     values = (ack,seq,bytes_data)
     UDP_Data = struct.Struct('I I 8s')
     packed_data = UDP_Data.pack(*values)
     chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-                                                                                                                                        
-    #Build the UDP Packet
-    values = (ack,seq,bytes_data,chksum)
-    UDP_Packet_Data = struct.Struct('I I 8s 32s')
-    UDP_Packet = UDP_Packet_Data.pack(*values)
-    return UDP_Packet
+    return chksum
 
 def changeSeq(seq):
     if seq == 0 :
@@ -83,13 +90,4 @@ def changeSeq(seq):
     else:
         seq = 0
     return seq
-
-def Packet_Checksum_Corrupter(packetdata):
-    if True and random.choice([0,1,0,1]) == 1: #  # Set to False to disable Packet Corruption. Default is 50% packets are corrupt
-        print("packet corrupt")
-        return(b'Corrupt!')
-    else:
-        return(packetdata)
-
-
 main()
--- CS3357/asn3/UDP_server.py
+++ CS3357/asn3/UDP_server.py
@@ -34,43 +34,50 @@ def main():
         data = UDP_Packet[2]
         packet_chksum = UDP_Packet[3]
         # #Network functions
-        Network_Delay()
         data = Packet_Checksum_Corrupter(data)
         loss = Network_Loss()
         if loss==1:
             #do not send packet
             pass
         else:
-            #Create the Checksum for comparison
-            values = (ack,seq,data)
-            packer = struct.Struct('I I 8s')
-            packed_data = packer.pack(*values)
-            chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-            #Compare Checksums to test for corrupt data
-            if packet_chksum == chksum:
-                print('CheckSums Match, Packet OK')
-                UDP_Packet = createACK(respAck,seq)
-                curr_sequence = changeSeq(curr_sequence)
-                sock.sendto(UDP_Packet,addr)
-                print("ack resp sent:",UDP_Packet)
+            if seq==curr_sequence:
+                #Create the Checksum for comparison
+                values = (ack,seq,data)
+                packer = struct.Struct('I I 8s')
+                packed_data = packer.pack(*values)
+                chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+                #Compare Checksums to test for corrupt data
+                if packet_chksum == chksum:
+                    print('CheckSums Match, Packet OK')
+                    UDP_Packet = createACK(respAck,seq,data)
+                    curr_sequence = changeSeq(curr_sequence)
+                    Network_Delay()
+                    sock.sendto(UDP_Packet,addr)
+                    print("ack resp sent:",UDP_Packet)
 
+                else:
+                    print('Checksums Do Not Match, Packet Corrupt')
+                    temp_sequence = changeSeq(curr_sequence)
+                    UDP_Packet = createACK(respAck,temp_sequence,data)
+                    Network_Delay()
+                    sock.sendto(UDP_Packet,addr)
+                    print("nak resp sent:",UDP_Packet)
             else:
-                print('Checksums Do Not Match, Packet Corrupt')
-                temp_sequence = changeSeq(curr_sequence)
-                UDP_Packet = createACK(respAck,temp_sequence)
+                UDP_Packet = createACK(respAck,curr_sequence,data)
+                Network_Delay()
                 sock.sendto(UDP_Packet,addr)
-                print("nak resp sent:",UDP_Packet)
+                print("ack resp resent:",UDP_Packet)
 
     sock.close()
-def createACK(respAck,seq): #creates the ACK packet to return
+def createACK(respAck,seq, data): #creates the ACK packet to return
     #Create the Checksum to return
-    response_values = (respAck,seq)
-    response_packer = struct.Struct('I I')
+    response_values = (respAck,seq, data)
+    response_packer = struct.Struct('I I 8s')
     response_packed_data = response_packer.pack(*response_values)
     chksum =  bytes(hashlib.md5(response_packed_data).hexdigest(), encoding="UTF-8")
     #create packet to return
-    packet_values = (respAck,seq,chksum)
-    UDP_Packet_Data = struct.Struct('I I 32s')
+    packet_values = (respAck,seq,data,chksum)
+    UDP_Packet_Data = struct.Struct('I I 8s 32s')
     UDP_Packet = UDP_Packet_Data.pack(*packet_values)
     return UDP_Packet
 
@@ -83,21 +90,21 @@ def changeSeq(seq): #changes the sequence number
 
 ### Network Functions ###
 def Network_Delay():
-    if False and random.choice([0,1,0]) == 1: # Set to False to disable Network Delay. Default is 33% packets are delayed
+    if True and random.choice([0,1,0]) == 1: # Set to False to disable Network Delay. Default is 33% packets are delayed
        time.sleep(.01)
        print("Packet Delayed")
     else:
         print("Packet Sent")
 
 def Network_Loss():
-    if False and random.choice([0,1,1,0]) == 1: # Set to False to disable Network Loss. Default is 50% packets are lost
+    if True and random.choice([0,1,1,0]) == 1: # Set to False to disable Network Loss. Default is 50% packets are lost
         print("Packet Lost")
         return(1)
     else:
         return(0)
 
 def Packet_Checksum_Corrupter(packetdata):
-     if False and random.choice([0,1,0,1]) == 1: #  # Set to False to disable Packet Corruption. Default is 50% packets are corrupt
+     if True and random.choice([0,1,0,1]) == 1: #  # Set to False to disable Packet Corruption. Default is 50% packets are corrupt
         print("packet corrupt")
         return(b'Corrupt!')
      else:
