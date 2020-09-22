812377c46f3d45eb79a9f8d973bbf4123b677e31
Dat Huu Vo <dvo9@uwo.ca>
2020-09-21 22:53:40 -0400

    test

--- math3159/asn1/test.py
+++ math3159/asn1/test.py
@@ -1,39 +1,51 @@
-def modInverse(a, m) : 
-    m0 = m 
-    y = 0
-    x = 1
-  
-    if (m == 1) : 
-        return 0
-  
-    while (a > 1) : 
 
-        # q is quotient 
-        q = a // m 
-        t = m 
-  
-        # m is remainder now, process 
-        # same as Euclid's algo 
-        m = a % m 
-        a = t 
-        t = y 
-  
-        # Update x and y 
-        y = x - q * y 
-        x = t 
+# number sets given through generate input 
+# x1, y1 = (32416188767, 32241382686) 
+# x2, y2 = (1050809276997196863379, 32416188127) 
+# x3, y3 = (970516904892, 955775357338)
 
-        print(a)
-  
-    # Make x positive 
-    if (x < 0) : 
-        x = x + m0 
-  
-    return x 
-  
-  
-# Driver program to test above function 
-a = 15
-m = 56
+container = [(56,15), (651161546456454643531,456456454664), (32416188767, 32241382686), (1050809276997196863379, 32416188127) ,(970516904892, 955775357338)
+            ]
 
-print("Modular multiplicative inverse is", modInverse(a, m)) 
+#   solve function
+def solve(m, a):
+    u = 1
+    g = a
+    x=0
+    y=m
+    if(m==1):
+        return 1,1,1
+    while True:
+        if(y==0):
+            v = (g-(a*u))/m
+            return g, u, v
+        else:
+            t = g % y 
+            q = g//y
+            s=u-(q*x)
+            u = x 
+            g=y
+            x=s
+            y=t
+
+    
+
+#test
+
+
+for i in container:
+    m = i[0]
+    a = i[1]
+    g,u,v = solve(m, a)
+    if (u<0):
+        u+=m
+    if (#u!=1 #and v!=1 and v>0
+    ):
+        #print("the inverse of {} (mod  {}) is {}".format(a,m,u))
+        print("g: {} u: {} v: {}".format(g,u,v))
+
+    else:
+        print("g: {} u: {} v: {}".format(g,u,v))
+
+        #print("{} is not invertable (mod {})".format(a,m))
 
