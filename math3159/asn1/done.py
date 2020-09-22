1682bb2e13d74e8990479b2db0bdbc9a882c5888
Dat Vo <at-vo@github.com>
2020-09-21 23:03:40 -0400

    done test

--- math3159/asn1/done.py
+++ math3159/asn1/done.py
@@ -1,15 +1,14 @@
 
-# number sets given through generate input 323
-# x1, y1 (32416188767, 32241382686) 
+# number sets given through generate input 
+# (32416188767, 32241382686) 
 # x2, y2 = (1050809276997196863379, 32416188127) 
 # x3, y3 = (970516904892, 955775357338)
 
 container = [   
-            # tests
-            (32416190039, 23716567735), (1050809318036091666301, 32416188517), (831018615847, 746527923770), 
-            (32416188839, 20727961657), (1050809325491815514539, 32416189031), (637009706803, 455514560204),
-            #323
-            (32416188767, 32241382686), (1050809276997196863379, 32416188127) ,(970516904892, 955775357338)
+                (56,15), 
+                #(32416188767, 32241382686), 
+                #(1050809276997196863379, 32416188127) ,
+                #(970516904892, 955775357338)
             ]
 
 #   solve function
@@ -17,8 +16,11 @@ def solve(m, a):
     trackA = 0                              # init reference to a
     result = 1                              # init result
     mod = m                                 # keep reference to original m
+
+    if m == 1: 
+        return 0
   
-    while True:     
+    while a > 1:     
         floor = a // m                      # calculate floor
 
         temp = m                            # begin switching m and a
@@ -26,25 +28,20 @@ def solve(m, a):
         a = temp                            # a is now m for next iteration   
 
         temp = trackA                       # temp is new a
-        trackA = result - (temp * floor)    # calculate a used for next iteration
+        trackA = result - (trackA * floor)  # calculate a used for next iteration
         result = temp                       # result is now new a
 
-        if a == 1:
-            break                           # finish when a is one
 
     if result < 0 : 
         result += mod                       # make result positive
   
     return result 
 
-
-
 #test
 for i in container:
     m = i[0]
     a = i[1]
     try:
-        solution = solve(m, a)
-        print("the inverse of {} (mod  {}) is {}".format(a,m,solution))
+        print("the inverse of {} (mod  {}) is {}".format(a,m,solve(m, a)))
     except ZeroDivisionError as ex:
         print("{} is not invertable (mod {})".format(a,m))
--- math3159/asn1/test.py
+++ math3159/asn1/test.py
@@ -4,48 +4,43 @@
 # x2, y2 = (1050809276997196863379, 32416188127) 
 # x3, y3 = (970516904892, 955775357338)
 
-container = [(56,15), (651161546456454643531,456456454664), (32416188767, 32241382686), (1050809276997196863379, 32416188127) ,(970516904892, 955775357338)
+container = [
+            # tests
+            #(32416190039, 23716567735), (1050809318036091666301, 32416188517), (831018615847, 746527923770), 
+            #(32416188839, 20727961657), (1050809325491815514539, 32416189031), (637009706803, 455514560204),
+            #323
+            (32416188767, 32241382686), (1050809276997196863379, 32416188127) ,(970516904892, 955775357338)
             ]
 
 #   solve function
 def solve(m, a):
     u = 1
     g = a
-    x=0
-    y=m
-    if(m==1):
+    x = 0
+    y = m
+    if(m == 1):
         return 1,1,1
     while True:
-        if(y==0):
-            v = (g-(a*u))/m
+        if(y == 0):
+            v = (g - ( a * u )) / m
             return g, u, v
         else:
             t = g % y 
             q = g//y
-            s=u-(q*x)
+            s = u-(q*x)
             u = x 
-            g=y
-            x=s
-            y=t
-
-    
-
+            g = y
+            x = s
+            y = t
 #test
-
-
 for i in container:
     m = i[0]
     a = i[1]
     g,u,v = solve(m, a)
     if (u<0):
         u+=m
-    if (#u!=1 #and v!=1 and v>0
-    ):
-        #print("the inverse of {} (mod  {}) is {}".format(a,m,u))
-        print("g: {} u: {} v: {}".format(g,u,v))
-
+    if (g==1):
+        print("the inverse of {} (mod  {}) is {}".format(a,m,u))
     else:
-        print("g: {} u: {} v: {}".format(g,u,v))
-
-        #print("{} is not invertable (mod {})".format(a,m))
+        print("{} is not invertable (mod {})".format(a,m))
 
