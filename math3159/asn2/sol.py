e6b0aff37941aef09fe60ed7a337048c117c096b
Dat Huu Vo <dvo9@uwo.ca>
2020-10-04 17:48:49 -0400

    done?

---
+++ math3159/asn2/sol.py
@@ -0,0 +1,48 @@
+from math import ceil, sqrt
+
+
+# container = [   
+#                 (8389057, 50354, 641736), 
+#                 (8389099, 81194, 4611664), 
+#                 (8391037, 68226, 7343254)
+#             ]
+
+container = [   
+                #( 13 , 6 , 5 ),             #9
+                #( 7 , 2 , 5 ),            #no
+                (17389, 9704, 13896),   #1159
+                #(59, 3, 19)
+            ]
+
+def bsgs(p,g,h):
+    '''
+    Solve for x in h = g^x mod p given a prime p.
+    If p is not prime, you shouldn't use BSGS anyway.
+    '''
+    N = ceil(sqrt(p - 1))  # phi(p) is p-1 if p is prime
+
+    # Store hashmap of g^{1...m} (mod p). Baby step.
+    tbl = {pow(g, i, p): i for i in range(N)}
+
+    # Precompute via Fermat's Little Theorem
+    c = pow(g, N * (p - 2), p)
+
+    #print(tbl)
+    # Search for an equivalence in the table. Giant step.
+    print(tbl)
+    list = []
+    for j in range(N):
+        y = (h * pow(c, j, p)) % p
+        list.append(y)
+        # if y in tbl:
+        #     print(j,y)
+        #     return j * N + tbl[y]
+    print(list)
+    # Solution not found
+    return None
+
+for i in container:
+    p = i[0]
+    g = i[1]
+    h = i[2]
+    print(bsgs(p,g,h))
--- math3159/asn2/solve.py
+++ math3159/asn2/solve.py
@@ -1,17 +1,17 @@
 #323
-container = [   
-                (8389057, 50354, 641736), 
-                (8389099, 81194, 4611664), 
-                (8391037, 68226, 7343254)
-            ]
+# container = [   
+#                 (8389057, 50354, 641736), 
+#                 (8389099, 81194, 4611664), 
+#                 (8391037, 68226, 7343254)
+#             ]
 
 
 #test
 container = [   
-                #( 13 , 6 , 5 ),             #9
-                 #( 7 , 2 , 5 ),            #no
-                 #( 17389 , 9704 , 13896)   #1159
-                 (59, 3, 19)
+                ( 13 , 6 , 5 ),             #9
+                ( 7 , 2 , 5 ),            #no
+                (17389, 9704, 13896),   #1159
+                (59, 3, 19)
             ]
 
 
@@ -19,17 +19,22 @@ def solve(p,g,h):
     M = 1 + int(p**(1/2))
     list1 = []
     list2 = []
-    n = -1
-    for i in range (M+1):
-        list1.append(g**i)
-        list2.append(h*(g**(-i*M)))
-        
+    for i in range (M + 1):
+        # store g^{0...M}
+        list1.append(g ** i % p)
+        # using fermat's little theorem to get a number u
+        flt = g ** (M * (p-2)) % p
+        u = flt ** i % p 
+        list2.append(h * u % p)
+    #print(list1)
+    #print(list2)
+    for i in list2:
+        if i in list1:
+            return list1.index(i) + list2.index(i) * M
 
-    for i in range(M+1):
-        if list1[i] % p == list2[i] % p:
-            return i + (i * M)
-    
 
+    # if solution not found
+    return None
 
 
 for i in container:
@@ -37,7 +42,7 @@ for i in container:
     g = i[1]
     h = i[2]
     solution = solve(p,g,h)
-    # if (solution != -1):
-    print("x = {} is a solution".format(solution))
-    # else:
-    #     print("there is no solution")
+    if (solution != None):
+        print("x = {} is a solution".format(solution))
+    else:
+        print("there is no solution")
